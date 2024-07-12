d10047b4782dd0f73fe12b1154e02495ea0a8cee
ADundar <atsd34@gmail.com>
2024-07-12 17:20:51 +0300

    made dns zone optional

--- dns.ts
+++ dns.ts
@@ -2,32 +2,30 @@ import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
 export class MeetVerseDNS extends pulumi.ComponentResource {
-    public gateway: gcp.dns.RecordSet;
-    public web: gcp.dns.RecordSet;
-    constructor(gw_ip: string, webHostname: string) {
-        super("Meetverse:DNS", "meetverse-dns");
-        const config = new pulumi.Config();
-        const zoneId = config.require('dns_zone');
-
-        const dnsZone = gcp.dns.getManagedZone({name: zoneId});
-        const zoneName = dnsZone.then(envDnsZone => envDnsZone.name);
-
-        this.gateway = new gcp.dns.RecordSet("gateway", {
-            name: dnsZone.then(zone => `meetverse.gateway.${zone.dnsName}`),
-            type: 'A',
-            managedZone: zoneName,
-            rrdatas: [gw_ip]
-        });
-
-
-        this.web = new gcp.dns.RecordSet("web", {
-            name: `${webHostname}.`,
-            type: "CNAME",
-            managedZone: zoneName,
-            rrdatas: [this.gateway.name]
-        })
-
-
+  public gateway: gcp.dns.RecordSet | undefined;
+  public web: gcp.dns.RecordSet | undefined;
+  constructor(gw_ip: string, webHostname: string) {
+    super("Meetverse:DNS", "meetverse-dns");
+    const config = new pulumi.Config();
+    const zoneId = config.get("dns_zone");
+    if (!zoneId) {
+      return;
     }
+    const dnsZone = gcp.dns.getManagedZone({ name: zoneId });
+    const zoneName = dnsZone.then((envDnsZone) => envDnsZone.name);
 
+    this.gateway = new gcp.dns.RecordSet("gateway", {
+      name: dnsZone.then((zone) => `meetverse.gateway.${zone.dnsName}`),
+      type: "A",
+      managedZone: zoneName,
+      rrdatas: [gw_ip]
+    });
+
+    this.web = new gcp.dns.RecordSet("web", {
+      name: `${webHostname}.`,
+      type: "CNAME",
+      managedZone: zoneName,
+      rrdatas: [this.gateway.name]
+    });
+  }
 }
