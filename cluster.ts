3c44d980ec8df539d2473cc17969f670081d0e2b
ADundar <atsd34@gmail.com>
2024-07-12 16:50:19 +0300

    fixes about latest changes

--- cluster.ts
+++ cluster.ts
@@ -1,68 +1,83 @@
-import * as pulumi from '@pulumi/pulumi'
-import * as k8s from '@pulumi/kubernetes'
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
 import * as gcp from "@pulumi/gcp";
-import {NginxChart} from "./helm/nginx";
+import { NginxChart } from "./helm/nginx";
 import * as fs from "fs";
-import {templateSettings, template} from "lodash";
-import {CertManagerChart} from "./helm/cert-manager";
-import {Repository} from "@pulumi/gcp/artifactregistry";
-import {MeetverseChart} from "./helm/meetverse";
-
+import { templateSettings, template } from "lodash";
+import { CertManagerChart } from "./helm/cert-manager";
+import { Repository } from "@pulumi/gcp/artifactregistry";
+import { MeetverseChart } from "./helm/meetverse";
 
 templateSettings.interpolate = /\${([\s\S]+?)}/g;
 
 export class MeetVerseCluster extends pulumi.ComponentResource {
-    public nginxRelease: k8s.helm.v3.Release;
+  public nginxRelease: k8s.helm.v3.Release;
 
-    constructor(adminEmail: string, repository: Repository, webHostname: string, diskSize: number | undefined) {
-        super("Meetverse:Cluster", "cluster");
-        const name = "meetverse-cluster";
-        diskSize = diskSize || 100;
-        // Create a GKE cluster
-        const engineVersion = gcp.container.getEngineVersions().then(v => v.latestMasterVersion);
-        const cluster = new gcp.container.Cluster(name, {
-            initialNodeCount: 1,
-            removeDefaultNodePool: true,
-            minMasterVersion: engineVersion
+  constructor(
+    adminEmail: string,
+    repository: Repository,
+    webHostname: string,
+    diskSize: number | undefined
+  ) {
+    super("Meetverse:Cluster", "cluster");
+    const name = "meetverse-cluster";
+    diskSize = 100;
+    // Create a GKE cluster
+    const engineVersion = gcp.container
+      .getEngineVersions()
+      .then((v) => v.latestMasterVersion);
+    const cluster = new gcp.container.Cluster(name, {
+      initialNodeCount: 1,
+      removeDefaultNodePool: true,
+      minMasterVersion: engineVersion
+    });
+
+    new gcp.container.NodePool("meetverse-node-pool", {
+      cluster: cluster.name,
+      initialNodeCount: 2,
+      autoscaling: {
+        minNodeCount: 0,
+        maxNodeCount: 4
+      },
+      nodeConfig: {
+        machineType: "n1-standard-2",
+        diskSizeGb: 100,
+        oauthScopes: [
+          "https://www.googleapis.com/auth/compute",
+          "https://www.googleapis.com/auth/devstorage.read_only",
+          "https://www.googleapis.com/auth/logging.write",
+          "https://www.googleapis.com/auth/monitoring"
+        ]
+      }
+    });
+
+    const kubeconfig = pulumi
+      .all([cluster.name, cluster.endpoint, cluster.masterAuth])
+      .apply(([name, endpoint, masterAuth]) => {
+        const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+        const data = fs.readFileSync("./tpl/kubeconfig", "utf8");
+        let compiled = template(data);
+        return compiled({
+          certificate: masterAuth.clusterCaCertificate,
+          endpoint: endpoint,
+          context: context
         });
+      });
 
-        new gcp.container.NodePool("meetverse-node-pool", {
-            cluster: cluster.name,
-            initialNodeCount: 2,
-            autoscaling: {
-                minNodeCount: 0,
-                maxNodeCount: 4,
-            },
-            nodeConfig: {
-                machineType: "n1-standard-2",
-                diskSizeGb: diskSize,
-                oauthScopes: [
-                    "https://www.googleapis.com/auth/compute",
-                    "https://www.googleapis.com/auth/devstorage.read_only",
-                    "https://www.googleapis.com/auth/logging.write",
-                    "https://www.googleapis.com/auth/monitoring"
-                ],
-            },
-        });
+    const clusterProvider = new k8s.Provider(name, {
+      kubeconfig: kubeconfig
+    });
 
-        const kubeconfig = pulumi.
-        all([ cluster.name, cluster.endpoint, cluster.masterAuth ]).
-        apply(([ name, endpoint, masterAuth ]) => {
-            const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
-            const data = fs.readFileSync("./tpl/kubeconfig","utf8");
-            let compiled = template( data );
-            return compiled( {certificate: masterAuth.clusterCaCertificate, endpoint: endpoint, context: context} );
-        });
+    const nginx = new NginxChart(clusterProvider);
+    this.nginxRelease = nginx.release;
 
-        const clusterProvider = new k8s.Provider(name, {
-            kubeconfig: kubeconfig,
-        });
+    new CertManagerChart(clusterProvider, adminEmail);
 
-        const nginx = new NginxChart(clusterProvider);
-        this.nginxRelease = nginx.release;
-
-        new CertManagerChart(clusterProvider, adminEmail);
-
-        new MeetverseChart(clusterProvider, repository, nginx.annotations, webHostname)
-    }
+    new MeetverseChart(
+      clusterProvider,
+      repository,
+      nginx.annotations,
+      webHostname
+    );
+  }
 }
--- secret.ts
+++ secret.ts
@@ -2,35 +2,36 @@ import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
 export type MeetVerseSecret = {
-    OPENAI_API_KEY: string
-    GOOGLE_CLIENT_ID: string
-    GOOGLE_CLIENT_SECRET: string
-    GMAIL_REFRESH_TOKEN: string
-    GMAIL_CLIENT_ID: string
-    GMAIL_CLIENT_SECRET: string
-    QDRANT_API_KEY: string
-    DD_API_KEY: string
-    MONGODB_URI: string
-    "mongodb-passwords": string
-    "mongodb-root-password": string
-}
+  OPENAI_API_KEY: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  QDRANT_API_KEY: string;
+  MONGODB_URI: string;
+  "mongodb-passwords": string;
+  "mongodb-root-password": string;
+};
 
 export class MeetverseSecrets extends pulumi.ComponentResource {
-    secretResource: k8s.core.v1.Secret
-    constructor(namespace: string, secretValues: MeetVerseSecret) {
-        super("Meetverse:Secrets", "meetverse-secrets");
+  secretResource: k8s.core.v1.Secret;
+  constructor(
+    namespace: string,
+    secretValues: MeetVerseSecret,
+    k8sProvider?: pulumi.ProviderResource
+  ) {
+    super("Meetverse:Secrets", "meetverse-secrets");
 
-        this.secretResource = new k8s.core.v1.Secret("meetverse-secret", {
-            metadata: {
-                name: "meetverse",
-                namespace: namespace
-            },
-            // `data` field must contain base64-encoded strings.
-            // Use the `pulumi.secret` method to encrypt the secret before storing it in state.
-            stringData: secretValues
-        });
-
-
-
-    }
+    this.secretResource = new k8s.core.v1.Secret(
+      "meetverse-secret",
+      {
+        metadata: {
+          name: "meetverse",
+          namespace: namespace
+        },
+        // `data` field must contain base64-encoded strings.
+        // Use the `pulumi.secret` method to encrypt the secret before storing it in state.
+        stringData: secretValues
+      },
+      { provider: k8sProvider }
+    );
+  }
 }
